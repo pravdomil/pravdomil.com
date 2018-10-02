6b8cf9e861847a2597b3abbf6a0b4a461108f919
pravdomil <pravdomil.toman@gmail.com>
2018-10-02 16:24:40 +0200

    list all repos

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -40,10 +40,6 @@ function normalizeRepos(repos) {
   const topics = []
   // add repos to corresponding topics
   for (const repo of repos) {
-    if (!repo.description) {
-      continue
-    }
-
     const repoTopics = repo.repositoryTopics.nodes
     const repoTopicName = repoTopics.length ? repoTopics[0].topic.name : "uncategorized"
     const topic = getTopic(topics, repoTopicName)
