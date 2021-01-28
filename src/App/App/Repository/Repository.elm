1ea0d6f568b3c3a51e9f07a5457702a9d944736f
pravdomil <pravdomil.toman@gmail.com>
2021-01-28 15:22:02 +0100

    Repository.query

--- src/App/App/Repository/Repository.elm
+++ src/App/App/Repository/Repository.elm
@@ -28,6 +28,40 @@ type alias Repository =
 
 
 {-| -}
+query : String
+query =
+    """
+query {
+  viewer {
+    repositories(isFork: false, privacy: PUBLIC, first: 100) {
+      nodes {
+        name
+        description
+
+        url
+        homepageUrl
+        isArchived
+
+        viewerHasStarred
+        stargazers {
+          totalCount
+        }
+
+        repositoryTopics(first: 100) {
+          nodes {
+            topic {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+
+{-| -}
 additional : List Repository
 additional =
     [ { name = "Services"
