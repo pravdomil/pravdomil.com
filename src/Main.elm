4b2d6620a14a0b6313f3bc47b9e2ecc6efea66ef
pravdomil <pravdomil.toman@gmail.com>
2021-01-28 12:30:21 +0100

    Main module

---
+++ src/Main.elm
@@ -0,0 +1,20 @@
+module Main exposing (main)
+
+import App.App.App exposing (..)
+import App.App.App_ exposing (..)
+import Browser
+import Json.Decode as Decode
+
+
+{-| To create application.
+-}
+main : Program Decode.Value Model Msg
+main =
+    Browser.application
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        , onUrlRequest = UrlRequested
+        , onUrlChange = UrlChanged
+        }
